56e15b496dd2e36186e3dedddae4d13c4f2437e8
eproulx <w0816747@myscc.ca>
2024-11-19 22:30:44 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {TestComponent} from "./test/test.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
